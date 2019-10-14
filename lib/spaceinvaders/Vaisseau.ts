657e2ba59fd0abcb7d9f86f12d16a3b326fc4337
Paul Laborderie <paul.laborderie@gmail.com>
2019-10-14 23:06:55 +0200

    [UPDATE] - Add vitesse attribute to Vaisseau class - Refactor Vaisseau constructor

--- lib/spaceinvaders/Vaisseau.ts
+++ lib/spaceinvaders/Vaisseau.ts
@@ -1,35 +1,57 @@
 import Position from "./Position";
 import Dimension from "./Dimension";
 
+interface IVaisseau {
+  origine: Position;
+  dimension: Dimension;
+  vitesse: number;
+}
+
 export default class Vaisseau {
   private origine: Position;
   private dimension: Dimension;
+  private vitesse: number;
 
   public constructor(longueur: number, hauteur: number, x?: number, y?: number);
-  public constructor(origine: Position, dimension: Dimension);
+  public constructor(origine: Position, dimension: Dimension, vitesse?: number);
   public constructor(
     a1: number | Position,
     a2: number | Dimension,
     a3?: number,
     a4?: number,
   ) {
-    let origine;
-    let dimension;
-    if (typeof a1 === "number" && typeof a2 === "number") {
-      dimension = new Dimension(a1, a2);
-      if (typeof a3 === "number" && typeof a4 === "number") {
-        origine = new Position(a3, a4);
-      } else {
-        origine = new Position(0, 0);
-      }
-    } else if (a1 instanceof Position && a2 instanceof Dimension) {
-      origine = a1;
-      dimension = a2;
-    } else {
-      throw new Error("Invalid arguments");
-    }
+    let { dimension, origine, vitesse } = this.formatConstructorParams(a1, a2, a3, a4);
+    this.dimension = dimension
     this.origine = origine;
-    this.dimension = dimension;
+    this.vitesse = vitesse;
+  }
+
+  private formatConstructorParams(a1: number | Position, a2: number | Dimension, a3: number | undefined, a4: number | undefined) {
+    if (typeof a1 === "number" && typeof a2 === "number") {
+      return this.numbersConstructor(a1, a2, a3, a4);
+    }
+    else if (a1 instanceof Position && a2 instanceof Dimension) {
+      return this.classesConstructor(a1, a2);
+    }
+    else {
+      throw new Error("Incorrect params");
+    }
+  }
+
+  private numbersConstructor(longueur: number, hauteur: number, x: number = 0, y: number = 0): IVaisseau {
+    return {
+      dimension: new Dimension(longueur, hauteur),
+      origine: new Position(x, y),
+      vitesse: 1,
+    };
+  }
+
+  private classesConstructor(position: Position, dimension: Dimension, vitesse: number = 1): IVaisseau {
+    return {
+      dimension,
+      origine: position,
+      vitesse,
+    };
   }
 
   public abscisseLaPlusAGauche(): number {
@@ -61,11 +83,11 @@ export default class Vaisseau {
   }
 
   public seDeplacerVersLaDroite(): void {
-    this.origine.changerAbscisse(this.origine.abscisse() + 1);
+    this.origine.changerAbscisse(this.origine.abscisse() + this.vitesse);
   }
 
   public seDeplacerVersLaGauche(): void {
-    this.origine.changerAbscisse(this.origine.abscisse() - 1);
+    this.origine.changerAbscisse(this.origine.abscisse() - this.vitesse);
   }
 
   public positionner(x: number, y: number): void {
